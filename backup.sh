0f909b3e2e29041c201a47d21a3ba9a3093562c9
Jacob Ouellette <jackscripter45@gmail.com>
2019-03-10 11:17:11 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,24 +4,23 @@
 #################################################################
 #			 CONFIGURATION 				#
 #################################################################
-# Place all folders you want to backup followed by the backup folder name. DON'T put * to select many folders.
+# Sendmail path:
+SENDMAIL='/usr/sbin/sendmail'
+
+# Place all folders you want to backup followed by the destination folder name. DON'T put * to select many folders.
 TOBACKUP=(
 	"/folder1" "fold1"
 	"/folder2/folder" "folders"
 )
-
-# Destination
+# Destination path
 DESTINATION="/backup"
 
 # Log folder
 LOGFOLDER="/var/log/backup"
 
-# Mailx (heirloom-mail) configuration
+# Email configuration
 FROMMAIL="source@mail.com"
-SMTPSERVER="smtp.mail.com"
-SMTPPORT="587"
-PASSWORD=""
-DESTINATOR="destinator@mail.com"
+DESTINATOR="destination@mail.com"
 
 #################################################################
 #		PROCESSING (don't edit this part)		#
@@ -30,16 +29,18 @@ mBody="Backup start time: $(date +%H:%M)"
 for ((i=0; i<${#TOBACKUP[@]}; i++)) do
 	if [ $(( $i % 2 )) == 0 ];then
 		nameNum=$((i + 1))
-		if [ ! -f $DESTINATION/${TOBACKUP[$nameNum]} ];then
-			mkdir -p $DESTINATION/${TOBACKUP[$nameNum]}
-		fi
+		if [ ! -f $DESTINATION/${TOBACKUP[$nameNum]} ];then mkdir -p $DESTINATION/${TOBACKUP[$nameNum]}; fi # Create destination folder if don't exist.
 		echo '['$(date +%H:%M)']' Starting backup folder ${TOBACKUP[$i]} &>> $LOGFOLDER/$(date +%F).txt
 		cp -pruv ${TOBACKUP[$i]} $DESTINATION/${TOBACKUP[$nameNum]} >> $LOGFOLDER/$(date +%F).txt
 	fi
 done
-smtpSP=$SMTPSERVER:$SMTPPORT
+echo "Backup end time: $(date +%H:%M)" &>> $LOGFOLDER/$(date +%F).txt
 mSubject="Backup Summary - $(date +%F)"
 mAttache=$LOGFOLDER/$(date +%F).txt
 mBody="$mBody
-Backup end time: $(date +%H:%M)"
-echo "$mBody" | heirloom-mailx -r $FROMMAIL -a $mAttache -s $mSubject -S smtp=$smtpSP -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user=$FROMMAIL -S smtp-auth-password=$PASSWORD -S ssl-verify=ignore $DESTINATOR
+#Backup end time: $(date +%H:%M)"
+(printf "%s\n" \
+	"From: $FROMMAIL" \
+	"To: $DESTINATOR" \
+	"Subject: $mSubject" \
+	"`cat $mAttache`";) | $SENDMAIL "$DESTINATOR"
